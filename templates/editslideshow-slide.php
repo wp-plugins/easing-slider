8e96b4d8ee41c49b544c6670af12487933181337
MatthewRuddy <MatthewRuddy@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-03-11 22:31:35 +0000

    Fixed backbone template issues. Please work! Oh god, haha!
    git-svn-id: https://plugins.svn.wordpress.org/easing-slider/branches/2.0.1.4-rc1@680077 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- templates/editslideshow-slide.php
+++ templates/editslideshow-slide.php
@@ -4,7 +4,7 @@
     <div class="media-modal-content">
         <div class="media-frame wp-core-ui">
             <div class="media-frame-title">
-                <h1><?php _e( 'Edit a Slide: #<%= id %>', 'easingsliderlite' ); ?></h1>
+                <h1><?php _e( 'Edit a Slide: #{{ data.id }}', 'easingsliderlite' ); ?></h1>
             </div>
 
             <div class="media-frame-router" style="height: 0;"></div>
@@ -14,30 +14,30 @@
                     <div id="general" class="media-tab">
                         <div class="embed-link-settings" style="top: 0;">
                             <div class="thumbnail">
-                                <% var size = sizes.large || sizes.medium || sizes.thumbnail || url; %>
-                                <img src="<%= size.url %>" class="slide-thumbnail" alt="<%= alt %>" />
+                                <# var size = data.sizes.large || data.sizes.medium || data.sizes.thumbnail || data.url; #>
+                                <img src="{{ data.url }}" class="slide-thumbnail" alt="{{ data.alt }}" />
                                 <a href="#" id="change-image" class="button button-primary button-large change-image" data-editor="content" title="Change Image"><span class="wp-media-buttons-icon"></span> Change Image</a>
                             </div>
 
                             <label for="link" class="setting">
                                 <span><?php _e( 'Link URL', 'easingsliderlite' ); ?></span>
-                                <input type="text" id="link" value="<%= link %>">
+                                <input type="text" id="link" value="{{ data.link }}">
                                 <select id="linkTarget" style="margin-top: 5px;">
-                                    <option value="_self" <% ( '_self' == linkTarget ) ? print('selected="selected"'): ''; %>><?php _e( 'Open link same tab/window', 'easingsliderlite' ); ?></option>
-                                    <option value="_blank" <% ( '_blank' == linkTarget ) ? print('selected="selected"'): ''; %>><?php _e( 'Open link in new tab/window', 'easingsliderlite' ); ?></option>
+                                    <option value="_self" <# ( '_self' == data.linkTarget ) ? print('selected="selected"'): ''; #>><?php _e( 'Open link same tab/window', 'easingsliderlite' ); ?></option>
+                                    <option value="_blank" <# ( '_blank' == data.linkTarget ) ? print('selected="selected"'): ''; #>><?php _e( 'Open link in new tab/window', 'easingsliderlite' ); ?></option>
                                 </select>
                                 <p class="description"><?php _e( 'Enter a URL to link this slide to another page.', 'easingsliderlite' ); ?></p>
                             </label>
 
                             <label for="title" class="setting">
                                 <span><?php _e( 'Title', 'easingsliderlite' ); ?></span>
-                                <input type="text" id="title" value="<%= title %>">
+                                <input type="text" id="title" value="{{ data.title }}">
                                 <p class="description"><?php _e( 'Enter a value for the image "title" attribute.', 'easingsliderlite' ); ?></p>
                             </label>
 
                             <label for="alt" class="setting">
                                 <span><?php _e( 'Alt Text', 'easingsliderlite' ); ?></span>
-                                <input type="text" id="alt" value="<%= alt %>">
+                                <input type="text" id="alt" value="{{ data.alt }}">
                                 <p class="description"><?php _e( 'Enter a value for the image "alt" text attribute.', 'easingsliderlite' ); ?></p>
                             </label>
                         </div>
